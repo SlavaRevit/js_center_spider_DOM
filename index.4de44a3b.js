148ae14ab7952a2c97dc9148b69f294b82543133
Slava Filimonenko <=>
2024-07-17 23:05:56 +0300

    make build

---
+++ index.4de44a3b.js
@@ -0,0 +1,2 @@
+var e,t,l,c,i,n;e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,c=e.clientHeight,i=t.clientWidth,n=t.clientHeight,t.style.position="relative",t.style.left="".concat(l/2-i/2,"px"),t.style.top="".concat(c/2-n/2,"px");
+//# sourceMappingURL=index.4de44a3b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+function centerTheSpider() {
+    const wall = document.querySelector(".wall");
+    const spider = document.querySelector(".spider");
+    const wallWidth = wall.clientWidth;
+    const wallHeight = wall.clientHeight;
+    const spiderWidth = spider.clientWidth;
+    const spiderHeight = spider.clientHeight;
+    spider.style.position = "relative";
+    spider.style.left = `${wallWidth / 2 - spiderWidth / 2}px`;
+    spider.style.top = `${wallHeight / 2 - spiderHeight / 2}px`;
+}
+centerTheSpider();
+
+//# sourceMappingURL=index.f75de5e1.js.map
